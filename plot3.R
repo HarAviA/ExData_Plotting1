0c4d4af26bd8c15f1185444241e1f1eb6e1e403c
HarAviA <74383886+HarAviA@users.noreply.github.com>
2021-01-10 17:31:28 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,12 @@
+## Plot 3
+
+# creating the plot
+
+par(mfrow = c(1,1), mar = c(4,4,2,1), oma = c(0,0,2,0))
+with(subsetdata, {
+  plot(Sub_metering_1 ~ Date_Time, type="l", ylab = "Energy Submetering", xlab="")
+  lines(Sub_metering_2 ~ Date_Time, col = "Red")
+  lines(Sub_metering_3 ~ Date_Time, col = "Blue")
+})
+legend("topright", col = c("black", "red", "blue"), lwd = c(1, 1, 1), 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
